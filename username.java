49902f14f0b1e28bff84bac14af3c4c82f986506
kishor <kishor@gmail.com>
2023-04-22 13:16:45 +0000

    this is username file

---
+++ username.java
@@ -0,0 +1,10 @@
+
+  //create label for password
+        passLabel = new JLabel();
+        passLabel.setText("Password");      //set label value for textField2
+
+        //create text field to get password from the user
+        textField2 = new JPasswordField(15);    //set length for the password
+
+        //create submit button
+        b1 = new JButton("SUBMIT"); //set label to button
